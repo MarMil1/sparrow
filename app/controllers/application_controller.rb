66aaa4b0933bbd508ffd730820f46e40f9927c84
marmil1 <milosavjevic.marko@outlook.com>
2024-05-29 23:58:05 -0500

    Implement user menu dropdown in navbar; edit users table.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 class ApplicationController < ActionController::Base
     def require_admin_user_logged_in!
-        redirect_to root_path, alert: "Unauthorized access." if current_user.roles.first.name != 'admin'
+        redirect_to root_path, alert: "Unauthorized access." if current_user.nil? || current_user.roles.first.name != 'admin'
+    end
+
+    # Upon Sign Out, redirect user to Sign In page in order to show the proper sign out message.
+    def after_sign_out_path_for(resource_or_scope)
+        new_user_session_path
     end
 end
