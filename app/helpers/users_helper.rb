a165e7667081e44cc8d6d42f25ce3898a5747e68
marmil1 <milosavjevic.marko@outlook.com>
2024-06-18 23:45:58 -0500

    Edit general styling/add borders.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,25 @@
 module UsersHelper
     def lock_status(user)
         if user.access_locked?
-            "#{heroicon("lock-open")} Unlock user account".html_safe
+            "#{heroicon("lock-open")} Unlock user".html_safe
         else
-            "#{heroicon("lock-closed")} Lock user account".html_safe
+            "#{heroicon("lock-closed")} Lock user".html_safe
+        end
+    end
+
+    def lock_status_dialog_confirmation_title(user)
+        if user.access_locked?
+            "Unlock user #{user.email}"
+        else
+            "Lock user #{user.email}"
+        end
+    end
+
+    def lock_status_dialog_confirmation_content(user)
+        if user.access_locked?
+            "Are you sure you want to unlock this user account?"
+        else
+            "Are you sure you want to lock this user account?"
         end
     end
 end
